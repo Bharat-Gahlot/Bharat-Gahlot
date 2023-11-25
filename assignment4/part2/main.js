fcd55661527f2d76089b6b90e555867398be917d
Bharat-Gahlot <133807441+Bharat-Gahlot@users.noreply.github.com>
2023-11-24 19:33:52 -0500

    initial commit part2
    all files added and coding started in main.js

---
+++ assignment4/part2/main.js
@@ -0,0 +1,27 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const imagesArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+/* Declaring the alternative text for each image file */
+const imagesText = {
+    'pic1.jpg' : 'Human eye',
+    'pic2.jpg' : 'Wave textured marble',
+    'pic3.jpg' : 'Purple and white flowers',
+    'pic4.jpg' : 'Egyptian wall art',
+    'pic5.jpg' : 'Butterfly on leaf'
+  }
+
+/* Looping through images */
+
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
